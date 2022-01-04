9d32708587a0053c76860c879fb635544db7c68d
srokks <srokks@gmail.com>
2022-01-04 14:28:01 +0100

    Revert "name changes"
    This reverts commit b7f233a71a8c2c7bf324e1f771e7c74d6bc59db3.

--- coopHUD_sprites.lua
+++ coopHUD_sprites.lua
@@ -71,11 +71,18 @@ function coopHUD.getActiveItemSprite(player,slot)
         fi_charge = player:GetActiveCharge()
         this_sprite:SetFrame("EverythingJar", fi_charge +1)
     end
-    -- Jar of Wisps sprite set
-    if active_item == 685 then
-        --TODO: Jar of Wisp
+    -- TODO: Jar of Wisp
+    --if activeitem == 685 then
+    --    --TODO: anim frames
+    --    --TODO: get charges
+    --end
+    -- TODO:Urn of soul
+    if active_item == 640 then
+        fi_charge = 0
+        print(player:GetJarFlies())
+        --      --TODO: get charge of urn
+        this_sprite:SetFrame("SoulUrn", fi_charge) -- sets frame
     end
-
 
     return this_sprite
 end
@@ -94,7 +101,6 @@ function coopHUD.getItemChargeSprite(player,slot)
     sprite:SetFrame("ChargeBar", step)
 
 end
-
 function coopHUD.getTrinketSprite(player, trinket_pos)
     Anim = "gfx/ui/item.anm2"
     local trinketid = player:GetTrinket(trinket_pos)
