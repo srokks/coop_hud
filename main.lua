53ed17724ccdd303abd08910847dbcea1db4f63f
srokks <srokks@gmail.com>
2022-01-05 10:18:29 +0100

    new getChargeSprites implement

--- main.lua
+++ main.lua
@@ -323,11 +323,11 @@ function coopHUD.renderPlayer(player_no)
             coopHUD.players[player_no].sprites.second_active_charge:Render(Vector(anchor_top.X +second_item_x_off+8,anchor_top.Y +8), vector_zero, vector_zero)
         end
         if coopHUD.players[player_no].sprites.first_active then
-            -- First item charge render
+            -- First item  render
             coopHUD.players[player_no].sprites.first_active:Render(Vector(anchor_top.X+active_vector.X,anchor_top.Y+16),VECTOR_ZERO,VECTOR_ZERO)
         end
         if coopHUD.players[player_no].sprites.first_active_charge then
-            -- First item render
+            -- First item charge render
             coopHUD.players[player_no].sprites.first_active_charge:Render(Vector(anchor_top.X+active_vector.X+20,anchor_top.Y+16), VECTOR_ZERO, VECTOR_ZERO)
         end
     else
