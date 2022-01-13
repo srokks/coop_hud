89a9fb5b13a29bbcea3baebed0137bb4508d052e
srokks <srokks@gmail.com>
2022-01-13 19:09:00 +0100

    PoopSpells

--- coopHUD_globals.lua
+++ coopHUD_globals.lua
@@ -3,5 +3,6 @@ coopHUD.GLOBALS = {
     card_anim_path = "gfx/ui/hud_card_coop.anm2",
     pill_anim_path = "gfx/ui/hud_pills_coop.anm2",
     hearts_anim_path = "gfx/ui/ui_hearts.anm2",
-    charge_anim_path = "gfx/ui/activechargebar_coop.anm2"
+    charge_anim_path = "gfx/ui/activechargebar_coop.anm2",
+    poop_anim_path = "gfx/ui/ui_poops.anm2"
 }
