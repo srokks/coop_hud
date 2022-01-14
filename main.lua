8a1dbd805e1ef7cb50220c67e8eea07668d872d5
srokks <srokks@gmail.com>
2022-01-14 12:35:50 +0100

    added debug

--- main.lua
+++ main.lua
@@ -5,24 +5,5 @@ include("coopHUD_globals.lua")
 include("coopHUD_player_table_updates.lua")
 include("coopHUD_sprites.lua")
 include("coopHUD_render.lua")
----
--- OPTIONS SKETCH
-local onRender = true
---coopHUD.init()
 --
-players_no = 0
--- init
---function coopHUD.init()
---    coopHUD.updateAnchors()
---    players_no = Game():GetNumPlayers()-1
---    for i=0,players_no,1 do
---        coopHUD.updatePlayer(i)
---        print('Initiated ',i,'Player')
---    end
---end
---coopHUD.init()
-
- -- Updates all players info on entering new flor
-
-
-
+coopHUD.init() -- Debug: for mod rerun from game console
