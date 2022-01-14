1df6235f0afcc58bef8d5f98b6880de29fc9ec4b
srokks <srokks@gmail.com>
2022-01-14 11:16:11 +0100

    new renders skeletons

--- coopHUD_globals.lua
+++ coopHUD_globals.lua
@@ -6,3 +6,23 @@ coopHUD.GLOBALS = {
     charge_anim_path = "gfx/ui/activechargebar_coop.anm2",
     poop_anim_path = "gfx/ui/ui_poops.anm2"
 }
+coopHUD.players = {}
+coopHUD.players.config = {
+        players_no = 0,
+        [0] = {
+            anchor_top = 'top_right',
+            anchor_bot = 'bot_right',
+            mirrored = true,
+        },
+        [1] = {
+            anchor_top = 'top_right',
+            anchor_bot = 'bot_right',
+            mirrored = true,
+        },
+}
+coopHUD.anchors = {
+    top_left = Vector(0,0),
+    bot_left = Vector(0,0),
+    top_right = Vector(0,0),
+    bot_right = Vector(0,0),
+}
