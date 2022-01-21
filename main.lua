0f1c6c5723470ffed1aed4f558f0a509f95dcac4
srokks <srokks@gmail.com>
2022-01-21 10:54:15 +0100

    added console debug info

--- main.lua
+++ main.lua
@@ -7,3 +7,5 @@ include("coopHUD_sprites.lua")
 include("coopHUD_render.lua")
 --
 coopHUD.init() -- Debug: for mod rerun from game console
+print('Coop HUD loaded ')
+if  Game():GetHUD():IsVisible() then Game():GetHUD():SetVisible(false) end
