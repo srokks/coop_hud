c590c5785d170df578126f9738d4c41ec1f376fc
srokks <srokks@gmail.com>
2022-02-15 14:52:25 +0100

    v.0.4

--- main.lua
+++ main.lua
@@ -7,6 +7,5 @@ include("coopHUD_player_table_updates.lua")
 include("coopHUD_sprites.lua")
 include("coopHUD_player_table_updates.lua")
 include("coopHUD_render.lua")
-include("coopHUD_test.lua")
 --
 print('CoopHUD v.'..tostring(coopHUD.VERSION)..' successfully!')
