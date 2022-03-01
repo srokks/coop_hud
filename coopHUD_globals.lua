443502921cfd9ea059888603a95d3f5866887710
srokks <mzzfkPMM77mFeaQ>
2022-03-01 16:47:41 +0100

    main: debug delete

--- coopHUD_globals.lua
+++ coopHUD_globals.lua
@@ -1,4 +1,4 @@
-coopHUD.VERSION = '0.4.7-PROD' --DEBUG: test name for in game recognition
+coopHUD.VERSION = '0.5-STEAM' --DEBUG: test name for in game recognition
 coopHUD.showExtraInfo = false
 coopHUD.HUD_table = {}
 coopHUD.signals = {
--- coopHUD_mcm.lua
+++ coopHUD_mcm.lua
@@ -23,12 +23,12 @@ coopHUD:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, coopHUD.save_options)
 if ModConfigMenu then
 	local mod_name = "Coop HUD"
 	--= Used to reset the config, remove on retail.
-	local categoryToChange = ModConfigMenu.GetCategoryIDByName(mod_name)
-	if categoryToChange then
-		ModConfigMenu.MenuData[categoryToChange] = {}
-		ModConfigMenu.MenuData[categoryToChange].Name = tostring(mod_name)
-		ModConfigMenu.MenuData[categoryToChange].Subcategories = {}
-	end
+	--local categoryToChange = ModConfigMenu.GetCategoryIDByName(mod_name)
+	--if categoryToChange then
+	--	ModConfigMenu.MenuData[categoryToChange] = {}
+	--	ModConfigMenu.MenuData[categoryToChange].Name = tostring(mod_name)
+	--	ModConfigMenu.MenuData[categoryToChange].Subcategories = {}
+	--end
 	--/
 	ModConfigMenu.UpdateCategory(mod_name, {
 		Info = {
