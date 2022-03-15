b98cab41e173c50fdb751caaf53c462ddaa80aeb
srokks <mzzfkPMM77mFeaQ>
2022-03-15 12:41:34 +0100

    v0.5.2 STEAM RELEASE

--- coopHUD_globals.lua
+++ coopHUD_globals.lua
@@ -1,4 +1,4 @@
-coopHUD.VERSION = '0.5.4-PROD' --DEBUG: test name for in game recognition
+coopHUD.VERSION = '0.5.2-STEAM' --DEBUG: test name for in game recognition
 coopHUD.showExtraInfo = false
 coopHUD.HUD_table = {}
 coopHUD.signals = {
