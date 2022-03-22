67ab9b4fa8dc1850c99bc7653702cfdc5099f049
srokks <mzzfkPMM77mFeaQ>
2022-03-22 17:14:16 +0100

    main: fixed loading files

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ coopHUD = RegisterMod("Coop HUD", 1)
 ---
 include("coopHUD_globals.lua")
 include("coopHUD_mcm.lua")
-include("coopHUD_player_table_updates.lua")
 include("coopHUD_sprites.lua")
 include("coopHUD_player_table_updates.lua")
 include("coopHUD_table_callbacks.lua")
