69fb311bfbeb847fb284ec359b7d101df44cdeae
srokks <mzzfkPMM77mFeaQ>
2022-03-30 10:18:06 +0200

    main: includes rework

--- main.lua
+++ main.lua
@@ -3,9 +3,8 @@ coopHUD = RegisterMod("Coop HUD", 1)
 ---
 include("coopHUD_globals.lua")
 include("coopHUD_mcm.lua")
+include('coopHUD_player.lua')
 include("coopHUD_sprites.lua")
-include("coopHUD_player_table_updates.lua")
-include("coopHUD_table_callbacks.lua")
-include("coopHUD_render.lua")
+include("coopHUD_callbacks.lua")
 --
 print('CoopHUD v.'..tostring(coopHUD.VERSION)..' successfully!')
