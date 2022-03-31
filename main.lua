9b673ba424e9865ce4a27812c383f8b04b46e419
srokks <mzzfkPMM77mFeaQ>
2022-03-31 20:07:34 +0200

    main: import hud file

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ include("coopHUD_globals.lua")
 include("coopHUD_mcm.lua")
 include('coopHUD_player.lua')
 include("coopHUD_sprites.lua")
+include('coopHUD_hud.lua')
 include("coopHUD_callbacks.lua")
 --
 print('CoopHUD v.'..tostring(coopHUD.VERSION)..' successfully!')
