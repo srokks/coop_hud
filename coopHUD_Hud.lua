cd5f6044dd3a470598ffee4cbb647be03930cf02
srokks <mzzfkPMM77mFeaQ>
2022-03-31 20:22:44 +0200

    coopHUD.HUD: added run infos

--- coopHUD_Hud.lua
+++ coopHUD_Hud.lua
@@ -8,15 +8,12 @@ coopHUD.HUD.fonts.lua_mini_lined = Font()
 coopHUD.HUD.fonts.lua_mini_lined:Load("font/luaminioutlined.fnt")
 coopHUD.HUD.fonts.team_meat_10 = Font()
 coopHUD.HUD.fonts.team_meat_10:Load("font/teammeatfont10.fnt")
---Todo:render items
---Todo:render chances
---Todo:render streak
---Todo:render
-
 coopHUD.HUD.coins = coopHUD.RunInfo(coopHUD.RunInfo.COIN)
 coopHUD.HUD.bombs = coopHUD.RunInfo(coopHUD.RunInfo.BOMB)
 coopHUD.HUD.keys = coopHUD.RunInfo(coopHUD.RunInfo.KEY)
-
+coopHUD.HUD.beth = coopHUD.RunInfo(coopHUD.RunInfo.BETH)
+coopHUD.HUD.t_beth = coopHUD.RunInfo(coopHUD.RunInfo.T_BETH)
+coopHUD.HUD.poop = coopHUD.RunInfo(coopHUD.RunInfo.POOP)
 ---coopHUD.HUD.render - renders specifics to the game like no of coins/keys/bombs
 ---Todo: based on options.show_dest_info - show info about run destination
 ---Todo: based on options.show_difficulty
@@ -27,11 +24,5 @@ function coopHUD.HUD.render()
 	local color = KColor(1, 1, 1, 1)
 	local anchor = Vector(Isaac.GetScreenWidth() / 2, Isaac.GetScreenHeight() - 16) -- middle of screen
 	local text = ''
-	--
-	local is_tainted_blue_baby
-	local is_bethany
-	local is_tainted_bethany
-	print(coopHUD.RunInfo.COIN)
-	print(coopHUD.RunInfo.BOMB)
-	print(coopHUD.RunInfo.KEY)
+
 end
