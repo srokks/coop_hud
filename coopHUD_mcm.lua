d98661172e081d89eb57570fddfc33ee6f3dad48
srokks <srokks@gmail.com>
2022-05-13 20:22:08 +0200

    DEBUG: added debug info about retail and MCM load

--- coopHUD_mcm.lua
+++ coopHUD_mcm.lua
@@ -59,14 +59,13 @@ coopHUD:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, function()
 end)
 if ModConfigMenu then
 	local mod_name = "Coop HUD"
-	--= Used to reset the config, remove on retail.
+	--= DEBUG: Used to reset the config, remove on retail.
 	local categoryToChange = ModConfigMenu.GetCategoryIDByName(mod_name)
 	if categoryToChange then
 		ModConfigMenu.MenuData[categoryToChange] = {}
 		ModConfigMenu.MenuData[categoryToChange].Name = tostring(mod_name)
 		ModConfigMenu.MenuData[categoryToChange].Subcategories = {}
 	end
-	--/
 	ModConfigMenu.UpdateCategory(mod_name, {
 		Info = {
 			"coopHUD Settings.",
