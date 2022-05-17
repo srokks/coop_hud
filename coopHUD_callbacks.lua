4ff0e500093ddb916b2090da41c57fc592e90019
srokks <mzzfkPMM77mFeaQ>
2022-05-17 22:31:10 +0200

    BoC: added shift logic

--- coopHUD_callbacks.lua
+++ coopHUD_callbacks.lua
@@ -421,66 +421,78 @@ end, EntityType.ENTITY_KNIFE)
 -- INPUT TRIGGERS
 local btn_held = 0
 function coopHUD.inputs_signals()
-	-- Trigger for turning on/off coop hud on `H` key
-	if coopHUD.options.h_trigger then
-		if Input.IsButtonTriggered(Keyboard.KEY_H, 0) then
-			if coopHUD.options.onRender then
-				coopHUD.options.onRender = false
-			else
-				coopHUD.options.onRender = true
-			end
-		end
-	end
-	-- Trigger for turning on/off timer on `T` key
-	if Input.IsButtonTriggered(Keyboard.KEY_T, 0) then
-		if coopHUD.options.timer_always_on then
-			coopHUD.options.timer_always_on = false
-		else
-			coopHUD.options.timer_always_on = true
-		end
-	end
-	local mapPressed = false
-	for i = 0, Game():GetNumPlayers() - 1 do
-		local controller_index = Isaac.GetPlayer(i).ControllerIndex
-		local player_index = coopHUD.getPlayerNumByControllerIndex(controller_index)
-		if Input.IsActionPressed(ButtonAction.ACTION_MAP, controller_index) then
-			mapPressed = player_index
-		end
-		-- DROP ACTION
-		if Input.IsActionTriggered(ButtonAction.ACTION_DROP, controller_index) then
-			if coopHUD.players[player_index].entPlayer:GetPlayerType() == PlayerType.PLAYER_ISAAC_B then
-				if coopHUD.players[player_index].collectibles ~= nil then
-					local collectibles = {}
-					for i = 2, #coopHUD.players[player_index].collectibles do
-						table.insert(collectibles, coopHUD.players[player_index].collectibles[i])
-					end
-					table.insert(collectibles, coopHUD.players[player_index].collectibles[1])
-					coopHUD.players[player_index].collectibles = collectibles
-				end
-			end
-		end
-	end
-	-- MAP BUTTON
-	local pressTime = 0.5
-	if mapPressed then
-		btn_held = btn_held + 1 / 60
-		if btn_held > pressTime then
-			coopHUD.signals.map = mapPressed
-			coopHUD.Streak(true, coopHUD.Streak.FLOOR)
-			if btn_held > 1.5 then
-				if coopHUD.options.show_my_stuff then
-					coopHUD.Collectibles(coopHUD.players[coopHUD.signals.map])
-				end
-			end
-			coopHUD.players[mapPressed].signals.map_btn = true
-		end
-	else
-		if coopHUD.signals.map then
-			coopHUD.players[coopHUD.signals.map].signals.map_btn = false
-		end
-		coopHUD.signals.map = false
-		btn_held = 0
-	end
+    -- Trigger for turning on/off coop hud on `H` key
+    if coopHUD.options.h_trigger then
+        if Input.IsButtonTriggered(Keyboard.KEY_H, 0) then
+            if coopHUD.options.onRender then
+                coopHUD.options.onRender = false
+            else
+                coopHUD.options.onRender = true
+            end
+        end
+    end
+    -- Trigger for turning on/off timer on `T` key
+    if Input.IsButtonTriggered(Keyboard.KEY_T, 0) then
+        if coopHUD.options.timer_always_on then
+            coopHUD.options.timer_always_on = false
+        else
+            coopHUD.options.timer_always_on = true
+        end
+    end
+    local mapPressed = false
+    for i = 0, Game():GetNumPlayers() - 1 do
+        local controller_index = Isaac.GetPlayer(i).ControllerIndex
+        local player_index = coopHUD.getPlayerNumByControllerIndex(controller_index)
+        if Input.IsActionPressed(ButtonAction.ACTION_MAP, controller_index) then
+            mapPressed = player_index
+        end
+        -- DROP ACTION
+        if Input.IsActionTriggered(ButtonAction.ACTION_DROP, controller_index) then
+            if coopHUD.players[player_index].entPlayer:GetPlayerType() == PlayerType.PLAYER_ISAAC_B then
+                if coopHUD.players[player_index].collectibles ~= nil then
+                    local collectibles = {}
+                    for i = 2, #coopHUD.players[player_index].collectibles do
+                        table.insert(collectibles, coopHUD.players[player_index].collectibles[i])
+                    end
+                    table.insert(collectibles, coopHUD.players[player_index].collectibles[1])
+                    coopHUD.players[player_index].collectibles = collectibles
+                end
+            end
+            if coopHUD.players[player_index].entPlayer:GetPlayerType() == PlayerType.PLAYER_CAIN_B then
+                --shift player bag of crafting if have
+                if coopHUD.players[player_index].bag_of_crafting ~= nil then
+                    local new_bag = {}
+                    for i = 2, #coopHUD.players[player_index].bag_of_crafting do
+                        table.insert(new_bag, coopHUD.players[player_index].bag_of_crafting[i])
+                    end
+                    table.insert(new_bag, coopHUD.players[player_index].bag_of_crafting[1])
+                    coopHUD.players[player_index].bag_of_crafting = new_bag
+                end
+
+            end
+        end
+    end
+    -- MAP BUTTON
+    local pressTime = 0.5
+    if mapPressed then
+        btn_held = btn_held + 1 / 60
+        if btn_held > pressTime then
+            coopHUD.signals.map = mapPressed
+            coopHUD.Streak(true, coopHUD.Streak.FLOOR)
+            if btn_held > 1.5 then
+                if coopHUD.options.show_my_stuff then
+                    coopHUD.Collectibles(coopHUD.players[coopHUD.signals.map])
+                end
+            end
+            coopHUD.players[mapPressed].signals.map_btn = true
+        end
+    else
+        if coopHUD.signals.map then
+            coopHUD.players[coopHUD.signals.map].signals.map_btn = false
+        end
+        coopHUD.signals.map = false
+        btn_held = 0
+    end
 end
 -- MAIN RENDER
 function coopHUD.render()
