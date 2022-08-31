34b77ca05378af493332fd6d33cad2c096f802dd
srokks <srokks@gmail.com>
2022-08-31 17:58:53 +0200

    COOP-14:Battle signal in challenge room

--- callbacks/battle_signal.lua
+++ callbacks/battle_signal.lua
@@ -5,7 +5,12 @@ coopHUD:AddCallback(ModCallbacks.MC_POST_RENDER, function(self)
 	local r = Game():GetLevel():GetCurrentRoom()
 	if not r:IsClear() then
 		-- check if room ready
-		coopHUD.signals.on_battle = true
+		coopHUD.signals.on_battle = 0 -- battle
+		if r:GetBossID() > 0 then
+			coopHUD.signals.on_battle = 1 -- boss
+		end
+	elseif r:IsAmbushActive() then
+		coopHUD.signals.on_battle = 2 -- ambush
 	else
 		coopHUD.signals.on_battle = false -- reset signal
 	end
