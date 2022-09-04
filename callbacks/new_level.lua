c9c21f1c33f0df69d74277be147991be15bb70c0
srokks <srokks@gmail.com>
2022-09-04 14:28:29 +0200

    COOP-136:VarData:floor_items load logic

--- callbacks/new_level.lua
+++ callbacks/new_level.lua
@@ -1,5 +1,6 @@
 -- __________ New floor streak trigger
 coopHUD:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, function()
+	coopHUD.floor_custom_items = {}
 	coopHUD.Streak(false, coopHUD.Streak.FLOOR)
 	-- reloads hud icons for golden keys/bombs etc.
 	coopHUD.HUD.coins = coopHUD.RunInfo(coopHUD.RunInfo.COIN)
