fb3cb86dd28b9bc554cb9e612b17f34bc2deb5d7
srokks <srokks@gmail.com>
2022-09-07 19:12:50 +0200

    LangApi:pocket fixes

--- callbacks/use_pill.lua
+++ callbacks/use_pill.lua
@@ -3,9 +3,7 @@
 coopHUD:AddCallback(ModCallbacks.MC_USE_PILL, function(_, effect_no, entPlayer)
 	local player = coopHUD.Player.getByEntityIndex(entPlayer.Index)
 	if player then
-		local pill_sys_name = Isaac.GetItemConfig():GetPillEffect(effect_no).Name
-		pill_sys_name = string.sub(pill_sys_name, 2) --  get rid of # on front of
-		coopHUD.Streak(false, coopHUD.Streak.ITEM, coopHUD.langAPI.getPocketName(pill_sys_name), nil, true,
+		coopHUD.Streak(false, coopHUD.Streak.ITEM, coopHUD.langAPI.getPillNameByEffect(effect_no), nil, true,
 		               player.font_color)
 	end
 end)
