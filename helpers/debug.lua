1b7ebf73a061ad1e5dd509dae1dbbf9f5dbb9f31
srokks <srokks@gmail.com>
2022-11-10 08:57:33 +0100

    added debug files

--- helpers/debug.lua
+++ helpers/debug.lua
@@ -1,17 +1,9 @@
 function coopHUD.test()
+	auto_save()
+	--draw_on_screen('test ',120,2)
 	if coopHUD.players[1] == nil then
 		coopHUD.on_start(_, true)
 	end
-	-----@type
-	--local str = 'hud_scale: ' .. tostring(coopHUD.options.hud_scale)
-	--coopHUD.HUD.fonts.lua_mini_lined:DrawString(str, 120, 2, KColor(1, 1, 1, 1), 0, false)
-	--str = 'minimapi displaymode: ' .. tostring(MinimapAPI:GetConfig("DisplayMode"))
-	--coopHUD.HUD.fonts.lua_mini_lined:DrawString(str, 120, 8, KColor(1, 1, 1, 1), 0, false)
-	--sprite_render_test(coopHUD.players[1].inventory)
-	--poop_render_test(coopHUD.players[1])
-	local scale = Vector(coopHUD.options.hud_scale, coopHUD.options.hud_scale)
-	--local off = coopHUD.players[1].speed:render(Vector(100, 100), true, false, false, 1, scale)
-	--coopHUD.HUD.angel:render(Vector(100, 100), false, true, false, 1, scale)
 	if Input.IsButtonTriggered(Keyboard.KEY_B, 0) then
 		spawn_passive_collectible(5)
 	end
@@ -24,10 +16,11 @@ function coopHUD.test()
 end
 coopHUD:AddCallback(ModCallbacks.MC_POST_RENDER, coopHUD.test)
 function coopHUD.cmd(_, cmd, params)
-	--print('asdsadsasd',cmd,params)
+	if string.match(cmd, 'hud') then
+		--do whathever
+	end
 end
 coopHUD:AddCallback(ModCallbacks.MC_EXECUTE_CMD, coopHUD.cmd)
-
 function spawn_passive_collectible(quantity)
 	if quantity == nil then
 		quantity = 1
@@ -54,11 +47,17 @@ function spawn_random_pickups(quantity)
 end
 function auto_save()
 	---___INVENTORY AUTO SAVE FEATURE
+	local curTime = Game():GetFrameCount()
+	local msecs = curTime % 30 * (10 / 3) -- turns the millisecond value range from [0 to 30] to [0 to 100]
+	local secs = math.floor(curTime / 30) % 60
+	local mins = math.floor(curTime / 30 / 60) % 60
+	local hours = math.floor(curTime / 30 / 60 / 60) % 60
 	if secs % 5 == 0 and msecs == 0 then
-
-		--coopHUD.debug_str('Auto saved!::' .. time_string)
+		coopHUD.save_options()
+		--coopHUD.debug_str('Auto saved options!', true)
 	end
 end
+
 function change_player_types(player_type)
 	for _, player in pairs(coopHUD.players) do
 		player.entPlayer:ChangePlayerType(player_type)
@@ -110,4 +109,6 @@ function poop_render_test(player)
 	player.first_pocket:render(pos + Vector(off.X, 0), false, coopHUD.players_config.small.scale, false)
 	player.first_pocket:render(pos + Vector(0, off.Y), false, coopHUD.players_config.small.scale, false)
 end
-
+function draw_on_screen(str, pos_x, pos_y)
+	coopHUD.HUD.fonts.lua_mini_lined:DrawString(tostring(str), pos_x, pos_y, KColor(1, 1, 1, 1), 0, false)
+end
